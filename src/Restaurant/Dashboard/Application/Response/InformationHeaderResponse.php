7c6da84485feaaced2e55f2121dc334b2d1400eb
Gibmyx <gibmyxjobs@gmail.com>
2021-12-21 10:48:28 -0400

    vista de dashboard header v2

--- src/Restaurant/Dashboard/Application/Response/InformationHeaderResponse.php
+++ src/Restaurant/Dashboard/Application/Response/InformationHeaderResponse.php
@@ -34,67 +34,37 @@ final class InformationHeaderResponse
 
     public function newUser(): array
     {
-        if ($this->newUser['lastMonth'] > 0)
-            $porcentaje = (($this->newUser['currentMonth'] - $this->newUser['lastMonth']) / $this->newUser['lastMonth']) * 100;
-
-        if ($this->newUser['lastMonth'] == 0)
-            $porcentaje = 100;
-
-        if ($this->newUser['lastMonth'] == 0 && $this->newUser['currentMonth'] == 0)
-            $porcentaje = 0;
-
-        return array_merge($this->newUser, [
-            "porcentaje" => round($porcentaje, 2),
-            "isPositive" => $porcentaje > 0
-        ]);
+        return $this->getCalculoPorcentaje($this->newUser);
     }
 
     public function reservation(): array
     {
-        if ($this->reservation['lastMonth'] > 0)
-            $porcentaje = (($this->reservation['currentMonth'] - $this->reservation['lastMonth']) / $this->reservation['lastMonth']) * 100;
-
-        if ($this->reservation['lastMonth'] == 0)
-            $porcentaje = 100;
-
-        if ($this->reservation['lastMonth'] == 0 && $this->reservation['currentMonth'] == 0)
-            $porcentaje = 0;
-
-        return array_merge($this->reservation, [
-            "porcentaje" => round($porcentaje, 2),
-            "isPositive" => $porcentaje > 0
-        ]);
+        return $this->getCalculoPorcentaje($this->reservation);
     }
 
     public function reservationsCompleted(): array
     {
-        if ($this->reservationsCompleted['lastMonth'] > 0)
-            $porcentaje = (($this->reservationsCompleted['currentMonth'] - $this->reservationsCompleted['lastMonth']) / $this->reservationsCompleted['lastMonth']) * 100;
-
-        if ($this->reservationsCompleted['lastMonth'] == 0)
-            $porcentaje = 100;
-
-        if ($this->reservationsCompleted['lastMonth'] == 0 && $this->reservationsCompleted['currentMonth'] == 0)
-            $porcentaje = 0;
-
-        return array_merge($this->reservationsCompleted, [
-            "porcentaje" => round($porcentaje, 2),
-            "isPositive" => $porcentaje > 0
-        ]);
+        return $this->getCalculoPorcentaje($this->reservationsCompleted);
     }
 
     public function reservationsCanceled(): array
     {
-        if ($this->reservationsCanceled['lastMonth'] > 0)
-            $porcentaje = (($this->reservationsCanceled['currentMonth'] - $this->reservationsCanceled['lastMonth']) / $this->reservationsCanceled['lastMonth']) * 100;
+        return $this->getCalculoPorcentaje($this->reservationsCanceled);
+    }
 
-        if ($this->reservationsCanceled['lastMonth'] == 0)
+    private function getCalculoPorcentaje($data): array
+    {
+
+        if ($data['lastMonth'] > 0)
+            $porcentaje = (($data['currentMonth'] - $data['lastMonth']) / $data['lastMonth']) * 100;
+
+        if ($data['lastMonth'] == 0)
             $porcentaje = 100;
 
-        if ($this->reservationsCanceled['lastMonth'] == 0 && $this->reservationsCanceled['currentMonth'] == 0)
+        if ($data['lastMonth'] == 0 && $data['currentMonth'] == 0)
             $porcentaje = 0;
 
-        return array_merge($this->reservationsCanceled, [
+        return array_merge($data, [
             "porcentaje" => round($porcentaje, 2),
             "isPositive" => $porcentaje > 0
         ]);
