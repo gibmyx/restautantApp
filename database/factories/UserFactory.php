f7d98a5224c59130f8c3a6ea738b6491d59295ef
Gibmyx <gibmyxjobs@gmail.com>
2021-12-26 10:10:24 -0400

    seeders admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,8 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => "Gibmyx Gomez",
-            'email' => "gibmyx@gmail.com",
+            'name' => "Administrador",
+            'email' => "admin@admin.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
