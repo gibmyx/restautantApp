3a87300a3f86ad4282375a8f599bd096177cca28
Gibmyx <gibmyxjobs@gmail.com>
2021-12-26 11:45:25 -0400

    env variable produccion seguro v2.2

--- apps/frontend/restaurant/Modules/LandingPage/Welcome.vue
+++ apps/frontend/restaurant/Modules/LandingPage/Welcome.vue
@@ -3,14 +3,14 @@
         <template #header_right>
             <ul v-if="$page.props.user" class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
-                    <inertia-link :href="route('dashboard')" class="nav-link">
+                    <inertia-link :href="urlApp" class="nav-link">
                         <i class="fas fa-home"></i><span class="nav-link-inner--text">Dashboard</span>
                     </inertia-link>
                 </li>
             </ul>
             <ul v-else class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
-                    <inertia-link :href="route('login')" class="nav-link">
+                    <inertia-link :href="`${urlApp}/login`" class="nav-link">
                         <i class="ni ni-key-25"></i><span class="nav-link-inner--text"> Login</span>
                     </inertia-link>
                 </li>
@@ -33,8 +33,15 @@ export default defineComponent({
 
     props: {},
 
+    data () {
+        return {
+            urlApp: null
+        }
+    },
 
     mounted() {
+        this.urlApp = process.env.MIX_APP_URL;
+
         let body = $("body");
 
         if (!body.hasClass("bg-default"))
