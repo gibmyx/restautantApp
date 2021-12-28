885988314625b55aa90218b481525b3634058449
Gibmyx <gibmyxjobs@gmail.com>
2021-12-28 16:09:20 -0400

    2

--- apps/backend/restaurant/Tables/Config/routes/api.php
+++ apps/backend/restaurant/Tables/Config/routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Apps\Backend\restaurant\Tables\Controllers\TablesGetController;
 
-Route::middleware(['auth:sanctum'])->put('/tables', TablesGetController::class);
+Route::middleware(['auth:sanctum'])->get('/tables', TablesGetController::class);
